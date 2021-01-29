96da9acc69bc550ee33a2db888e57c49d7efb6b2
Yon-lu Goh <yonlu@leadiq.com>
2021-01-29 12:06:27 +0800

    Run writefile

--- src/main/scala/exercises/WriteFile.scala
+++ src/main/scala/exercises/WriteFile.scala
@@ -1,15 +1,46 @@
-package exercises
+package write
 
 import zio.{Task, ZIO}
+import zio.console._
 
-object WriteFile {
+object WriteFile extends zio.App {
+  def run(args: List[String]): zio.URIO[zio.ZEnv, zio.ExitCode] =
+    copyAndWrite.exitCode
 
+  // 1
+  def readFile(file: String): String = {
+    val source = scala.io.Source.fromFile(file)
+    try source.getLines.mkString
+    finally source.close()
+  }
+
+
+  def readFileZio(file: String): Task[String] = ZIO.effect(readFile(file))
+
+  // 2
   def writeFile(file: String, text: String): Unit = {
     import java.io._
-    val pw = new PrintWriter(new File(file))
+    val fileObj: File = new File(file)
+    fileObj.createNewFile()
+    val pw = new PrintWriter(fileObj)
     try pw.write(text) finally pw.close
   }
 
-  def writeFileZio(file: String, text: String): Task[Unit] =
-    ZIO.effect(writeFile(file, text))
+  def writeFileZio(file: String, text: String): Task[Unit] = ZIO.effect(writeFile(file, text))
+
+  // 3
+  def copyFileZio(source: String, dest: String): Task[Unit] =
+    for {
+      content <- readFileZio(source)
+      _       <- writeFileZio(dest, content)
+    }
+  yield ()
+
+  def copyAndWrite =
+    for {
+      _ <- copyFileZio("build.sbt", "/tmp/build.sbt.cpy")
+      copied <- readFileZio("/tmp/build.sbt.cpy")
+      _ <- putStrLn(copied)
+    }
+  yield ()
 }
