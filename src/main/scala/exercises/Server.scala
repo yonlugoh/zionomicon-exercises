47cff2f01e21d1a76cd1f6cd5c31ca634f801ce9
Yon-lu Goh <yonlu@leadiq.com>
2021-03-22 17:25:06 +0800

    add todo

--- src/main/scala/exercises/Server.scala
+++ src/main/scala/exercises/Server.scala
@@ -4,6 +4,7 @@ import exercises.HttpApp.httpApp
 import org.http4s.server.blaze.BlazeServerBuilder
 import zio._
 
+// TODO: can't compile
 object Server extends zio.App {
   val server: ZManaged[Any, Nothing, Nothing] = ZIO.runtime[Any].toManaged_.flatMap { implicit runtime =>
     BlazeServerBuilder[Task](runtime.platform.executor.asEC)
